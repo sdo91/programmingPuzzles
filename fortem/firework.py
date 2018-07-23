0eadd83288e8b2e5b11e3dbc475611590c486485
Scott Oehler <scott.oehler@fortemtech.com>
2018-07-22 22:02:58 -0600

    add approx integral calculation

--- fortem/firework.py
+++ fortem/firework.py
@@ -27,7 +27,7 @@ find 2d area from center to end
 def main():
     print 'firework'
 
-    print calcY(5, 45 * DEG2RAD)
+    # print calcY(5, 45 * DEG2RAD)
 
     # x = 1e-3
     # bestTheta = givenXFindTheta(x)
@@ -38,12 +38,15 @@ def main():
     #     print x, bestTheta * RAD2DEG, calcY(x, bestTheta)
 
 
+    # approxSolution()
+    varyDeltaX()
 
 
-    plotRangeOfFixedThetas()
-    plotOptimal()
 
-    showPlots()
+
+    # plotRangeOfFixedThetas()
+    # plotOptimal()
+    # showPlots()
 
 
 
@@ -113,11 +116,82 @@ def givenXFindY(x):
     final function to integrate
     combine 'givenXFindTheta' and 'calcY'
 
-    NOTE: wolfram alpha reverses atan2 params
+    wolfram alpha version:
+    y = 100+(x/tan(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x)))-(9.81*x**2)/(800*sin(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x))**2)
+
+    x = 2000*sqrt(2362)/981
     """
     # theta_rad = 2 * math.atan2(math.sqrt(962361 * x**2 + 1.6e9) - 40000, 981 * x)
     return 100 + (x / math.tan(2 * math.atan2(math.sqrt(962361 * x ** 2 + 1.6e9) - 40000, 981 * x))) - (9.81 * x ** 2) / (800 * math.sin(2 * math.atan2(math.sqrt(962361 * x ** 2 + 1.6e9) - 40000, 981 * x)) ** 2)
 
+
+def finalEquation():
+    """
+    shell integration
+
+    V = 2 pi Integral(x * f(x)) dx from a to b
+    f(x) = 100+(x/tan(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x)))-(9.81*x**2)/(800*sin(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x))**2)
+    a = 0
+    b = 2000*sqrt(2362)/981
+
+
+    integrate 2 * pi * x * f(x) from 0 to (2000*sqrt(2362)/981)
+    integrate 2 * pi * x * (100+(x/tan(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x)))-(9.81*x**2)/(800*sin(2*atan2(sqrt(962361*x**2+1.6e9)-40000,981*x))**2)) from 0 to (2000*sqrt(2362)/981)
+
+    ans = 1.85653e6
+    """
+    pass
+
+def varyDeltaX():
+
+    dxValues = []
+    for x in range(5):
+        dxValues.append(1 * 10 ** -x)
+
+    print dxValues
+
+    for dx in dxValues:
+        print 'dx={}, vol={}'.format(dx, approxSolution(dx, False))
+
+
+def approxSolution(dx=None, verbose=True):
+
+    if not dx:
+        dx = 1e-3
+
+    lastXVal = 2000 * math.sqrt(2362) / 981
+    if verbose:
+        print 'lastXVal: {} (y={})'.format(lastXVal, givenXFindY(lastXVal))
+    xValues = np.arange(0, lastXVal, dx)
+
+    size = len(xValues)
+
+    totalVolume = 0
+
+    for i in xrange(size):
+        a = xValues[i]
+        b = a + dx
+        x = (a+b)/2
+        y = givenXFindY(x)
+
+        shellVol = 2 * math.pi * x * y * dx
+
+        if verbose:
+            if i < 5 or size - i < 5:
+                print 'shellVol at x={}, y={}: {}'.format(x, y, shellVol)
+
+        totalVolume += shellVol
+
+    if verbose:
+        print 'totalVolume:', totalVolume
+    return totalVolume
+
+
+
+    # for x in xValues:
+    #     # bestTheta = givenXFindTheta(x)
+    #     # print x, bestTheta * RAD2DEG, calcY(x, bestTheta)
+    #     plt.plot(x, givenXFindY(x), 'r.')
 
 
 def givenXFindTheta(x):
