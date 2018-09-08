a29fbac8ba8b7606d394bbf1cb7878fd9f3bbe75
sdo91 <sdo91@gmx.com>
2018-09-07 22:19:16 -0600

    added euler template

---
+++ project_euler/TEMPLATE.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import time
+
+
+"""
+
+"""
+def main():
+    print 'starting {}'.format(__file__.split('/')[-1])
+    startTime = time.time()
+
+
+    elapsedTime = time.time() - startTime
+    print 'elapsedTime: {:.2f} s'.format(elapsedTime)
+# end main
+
+### HELPER FUNCTIONS ###
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
