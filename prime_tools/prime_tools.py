e3f620e1ac8c9e90b9751179139269325780578e
sdo91 <sdo91@gmx.com>
2019-11-13 23:03:18 -0700

    python 3 stuff

--- prime_tools/prime_tools.py
+++ prime_tools/prime_tools.py
@@ -8,10 +8,14 @@ PRIMES_FOLDER = '/home/sdo91/code/programming_puzzles/prime_tools/primes/'
 
 class PrimeTools(object):
 
+    @staticmethod
+    def getRandomNumber():
+        return 4  # chosen by fair dice roll. guaranteed to be random.
+
     def getTenMilPrimesList(self, whichPart, numPrimesToLoad=10e6):
         result = []
         primesFile = PRIMES_FOLDER + '2T_part{}.txt'.format(whichPart)
-        print 'loading primes from {}'.format(primesFile)
+        print('loading primes from {}'.format(primesFile))
         with open(primesFile) as infile:
             i = 0
             for line in infile:
@@ -19,17 +23,17 @@ class PrimeTools(object):
                 if i * 10 > numPrimesToLoad:
                     break
                 if i % (numPrimesToLoad / 100) == 0:
-                    print '{}%'.format(int(i * 1000 / numPrimesToLoad)),
+                    print('{}%'.format(int(i * 1000 / numPrimesToLoad)),)
                 tokens = line.split()
                 for x in tokens:
                     result.append(int(x))
-            print '\nloaded {} primes'.format(len(result))
+            print('\nloaded {} primes'.format(len(result)))
         return result
 
     def getTenMilPrimesSet(self, whichPart, numPrimesToLoad=10e6):
         l = self.getTenMilPrimesList(whichPart, numPrimesToLoad)
         result = set(l)
-        print 'converted to set'
+        print('converted to set')
         return result
 
 
@@ -41,10 +45,10 @@ def main():
 
     pt = PrimeTools()
     tenMilPrimes_list = pt.getTenMilPrimesList(1)
-    print len(tenMilPrimes_list)
+    print(len(tenMilPrimes_list))
 
     elapsedTime = time.time() - startTime
-    print 'elapsedTime: {:.2f} s'.format(elapsedTime)
+    print('elapsedTime: {:.2f} s'.format(elapsedTime))
 
 if __name__ == '__main__':
     main()
--- project_euler/TEMPLATE.py
+++ project_euler/TEMPLATE.py
@@ -1,9 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import division
+def addToPath(relPath):
+    from os import path
+    import sys
+    dirOfThisFile = path.dirname(path.realpath(__file__))
+    dirToAdd = path.normpath(path.join(dirOfThisFile, relPath))
+    if dirToAdd not in sys.path:
+        print('adding to path: {}'.format(dirToAdd))
+        sys.path.insert(0, dirToAdd)
+    else:
+        print('already in path: {}'.format(dirToAdd))
+
+addToPath('./prime_tools')
 
 import time
-import primefac
+# import primefac
+import prime_tools
 from itertools import count
 from fractions import Fraction
 
@@ -12,12 +24,13 @@ from fractions import Fraction
 
 """
 def main():
-    print 'starting {}'.format(__file__.split('/')[-1])
+    print('starting {}'.format(__file__.split('/')[-1]))
     startTime = time.time()
 
+    assertEqual(2 + 2, 4)
 
     elapsedTime = time.time() - startTime
-    print 'elapsedTime: {:.2f} s'.format(elapsedTime)
+    print('elapsedTime: {:.2f} s'.format(elapsedTime))
 # end main
 
 ### HELPER FUNCTIONS ###
@@ -27,8 +40,8 @@ def main():
 
 def assertEqual(a, b):
     if a != b:
-        print 'a: {}'.format(a)
-        print 'b: {}'.format(b)
+        print('a: {}'.format(a))
+        print('b: {}'.format(b))
     assert a == b
 
 
