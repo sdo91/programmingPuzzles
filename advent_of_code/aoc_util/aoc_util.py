8cf8de8274290969977fb891741ce61e9ba21696
Scott Oehler <sdo91@gmx.com>
2019-12-01 12:49:37 -0700

    formatting

--- advent_of_code/aoc_util/aoc_util.py
+++ advent_of_code/aoc_util/aoc_util.py
@@ -1,7 +1,4 @@
 
-
-
-
 import re
 import typing
 
@@ -18,26 +15,34 @@ class AocLogger(object):
             else:
                 print(msg)
 
+
 def lmap(func, *iterables):
     return list(map(func, *iterables))
+
 
 def ints(s: str) -> typing.List[int]:
     return lmap(int, re.findall(r"-?\d+", s))  # thanks mserrano!
 
+
 def positive_ints(s: str) -> typing.List[int]:
     return lmap(int, re.findall(r"\d+", s))  # thanks mserrano!
+
 
 def floats(s: str) -> typing.List[float]:
     return lmap(float, re.findall(r"-?\d+(?:\.\d+)?", s))
 
+
 def positive_floats(s: str) -> typing.List[float]:
     return lmap(float, re.findall(r"\d+(?:\.\d+)?", s))
+
 
 def words(s: str) -> typing.List[str]:
     return re.findall(r"[a-zA-Z]+", s)
 
+
 def split_and_strip_each(s: str, c: str) -> typing.List[str]:
     return lmap(str.strip, s.split(c))
+
 
 def assert_equal(expected, actual):
     if expected != actual:
