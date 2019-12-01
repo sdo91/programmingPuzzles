935a7016c7ed9d7fc12ec90495cd2de19a023d23
Scott Oehler <sdo91@gmx.com>
2019-12-01 10:13:28 -0600

    aoc 2019 1

--- advent_of_code/2019/in_progress/2019_day_1.py
+++ advent_of_code/2019/in_progress/2019_day_1.py
@@ -25,9 +25,9 @@ import aoc_util
 ### constants ###
 TEST_INPUT = [
     """
-
+1969
     """, """
-
+100756
     """, """
 
     """
@@ -40,8 +40,8 @@ TEST_OUTPUT_1 = [
 ]
 
 TEST_OUTPUT_2 = [
-    0,
-    0,
+    966,
+    50346,
     0,
 ]
 
@@ -55,16 +55,16 @@ class AdventOfCode(object):
     def run(self):
         print('starting {}'.format(__file__.split('/')[-1]))
 
-        # puzzle_input = aocd.data
+        puzzle_input = aocd.data
 
         self.test_cases()
         # self.solve_part_1(puzzle_input)
-        # self.solve_part_2(puzzle_input)
+        self.solve_part_2(puzzle_input)
 
 
 
     def test_cases(self):
-        for test_in, test_out in zip(TEST_INPUT, TEST_OUTPUT_1):
+        for test_in, test_out in zip(TEST_INPUT, TEST_OUTPUT_2):
             test_in = test_in.strip()
             if not test_in:
                 continue
@@ -72,7 +72,7 @@ class AdventOfCode(object):
             # do the test
             aoc_util.assert_equal(
                 test_out,
-                self.solve_part_1(test_in)
+                self.solve_part_2(test_in)
             )
 
     def solve_part_1(self, puzzle_input):
@@ -82,7 +82,13 @@ class AdventOfCode(object):
 
         Returns: the answer
         """
-        return 0
+        lines = puzzle_input.strip().split()
+        result = 0
+        for line in lines:
+            result += (int(line) // 3) - 2
+
+        print('part 1 result: {}'.format(result))
+        return result
 
     def solve_part_2(self, puzzle_input):
         """
@@ -90,8 +96,28 @@ class AdventOfCode(object):
             puzzle_input (string): the input
 
         Returns: the answer
+
+        3395944 too low
+        5093873 too high
         """
-        return 0
+        lines = puzzle_input.strip().split()
+        total_fuel_needed = 0
+        for line in lines:
+            fuel_needed_this_module = int(line) // 3 - 2
+            prev_step_fuel_needed = fuel_needed_this_module
+
+            while True:
+                extra_fuel_needed = prev_step_fuel_needed // 3 - 2
+                if extra_fuel_needed < 1:
+                    break
+                print(extra_fuel_needed)
+                fuel_needed_this_module += extra_fuel_needed
+                prev_step_fuel_needed = extra_fuel_needed
+
+            total_fuel_needed += fuel_needed_this_module
+
+        print('part 2 result: {}'.format(total_fuel_needed))
+        return total_fuel_needed
 
 
 
