498b5d05af8abc5b164f7adf8c35810c9eb493ee
Scott Oehler <sdo91@gmx.com>
2019-12-08 19:29:13 -0700

    use instruction_ptr in get_out_idx

--- advent_of_code/aoc_util/intcode_computer.py
+++ advent_of_code/aoc_util/intcode_computer.py
@@ -13,7 +13,7 @@ class IntcodeComputer(object):
     def __init__(self, initial_memory: typing.List[int]):
         self.initial_memory = initial_memory
 
-        self.i_ptr = 0
+        self.instruction_ptr = 0
         self.memory = []
         self.input_ptr = 0
         self.input_list = []
@@ -23,7 +23,7 @@ class IntcodeComputer(object):
         self.reset()
 
     def reset(self):
-        self.i_ptr = 0
+        self.instruction_ptr = 0
         self.memory = self.initial_memory.copy()
 
         self.input_ptr = 0
@@ -38,8 +38,8 @@ class IntcodeComputer(object):
     def get_latest_output(self):
         return self.output_list[-1]
 
-    def get_out_idx(self, opcode_index, offset):
-        return self.memory[opcode_index + offset]
+    def get_out_idx(self, offset):
+        return self.memory[self.instruction_ptr + offset]
 
     def get_param(self, param_modes, offset):
         POSITION_MODE = 0
@@ -51,9 +51,9 @@ class IntcodeComputer(object):
             param_mode = 0
 
         if param_mode == POSITION_MODE:
-            param_addr = self.memory[self.i_ptr + offset]
+            param_addr = self.memory[self.instruction_ptr + offset]
         elif param_mode == IMMEDIATE_MODE:
-            param_addr = self.i_ptr + offset
+            param_addr = self.instruction_ptr + offset
         else:
             raise RuntimeError('invalid param_mode: {}'.format(param_mode))
 
@@ -66,14 +66,13 @@ class IntcodeComputer(object):
         """
 
         while True:
-            full_opcode = str(self.memory[self.i_ptr])
+            full_opcode = str(self.memory[self.instruction_ptr])
 
             opcode = int(full_opcode[-2:])
-            param_modes = []
 
+            param_modes = []
             for c in full_opcode[:-2]:
                 param_modes.insert(0, int(c))
-
 
             if opcode == 99:
                 self.state = self.STATE_HALTED
@@ -83,31 +82,32 @@ class IntcodeComputer(object):
                 # add
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
-                out = self.get_out_idx(self.i_ptr, 3)
+                out = self.get_out_idx(3)
                 self.memory[out] = a + b
-                self.i_ptr += 4
+                self.instruction_ptr += 4
 
             elif opcode == 2:
                 # mult
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
-                out = self.get_out_idx(self.i_ptr, 3)
+                out = self.get_out_idx(3)
                 self.memory[out] = a * b
-                self.i_ptr += 4
+                self.instruction_ptr += 4
 
             elif opcode == 3:
                 # input
-                out = self.get_out_idx(self.i_ptr, 1)
+                out = self.get_out_idx(1)
                 self.memory[out] = self.input_list[self.input_ptr]
                 self.input_ptr += 1
-                self.i_ptr += 2
+                self.instruction_ptr += 2
 
             elif opcode == 4:
                 # output
-                self.output_list.append(self.memory[self.memory[self.i_ptr+1]])
-                AocLogger.log('out: {}'.format(self.output_list[-1]))
-                self.i_ptr += 2
+                a = self.get_param(param_modes, 1)
+                self.output_list.append(a)
+                AocLogger.log('intcode output: {}'.format(self.output_list[-1]))
                 self.state = self.STATE_OUTPUT
+                self.instruction_ptr += 2
                 break
 
             elif opcode == 5:
@@ -115,34 +115,34 @@ class IntcodeComputer(object):
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
                 if a:
-                    self.i_ptr = b
+                    self.instruction_ptr = b
                 else:
-                    self.i_ptr += 3
+                    self.instruction_ptr += 3
 
             elif opcode == 6:
                 # jump if false
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
                 if not a:
-                    self.i_ptr = b
+                    self.instruction_ptr = b
                 else:
-                    self.i_ptr += 3
+                    self.instruction_ptr += 3
 
             elif opcode == 7:
                 # less than
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
-                out = self.get_out_idx(self.i_ptr, 3)
+                out = self.get_out_idx(3)
                 self.memory[out] = int(a < b)
-                self.i_ptr += 4
+                self.instruction_ptr += 4
 
             elif opcode == 8:
                 # eq
                 a = self.get_param(param_modes, 1)
                 b = self.get_param(param_modes, 2)
-                out = self.get_out_idx(self.i_ptr, 3)
+                out = self.get_out_idx(3)
                 self.memory[out] = int(a == b)
-                self.i_ptr += 4
+                self.instruction_ptr += 4
 
             else:
                 raise RuntimeError('invalid opcode: {}'.format(opcode))
