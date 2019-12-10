f1f1377c60d1ad9605a123b01c358bfe0659d4ca
Scott Oehler <sdo91@gmx.com>
2019-12-09 21:58:23 -0700

    intcode refactor

--- advent_of_code/aoc_util/intcode_computer.py
+++ advent_of_code/aoc_util/intcode_computer.py
@@ -83,30 +83,30 @@ class IntcodeComputer(object):
 
             elif opcode == 1:
                 # add
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
-                out = self._get_out_idx(3)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
+                out = self._get_idx(3)
                 self.memory[out] = a + b
                 self.instruction_ptr += 4
 
             elif opcode == 2:
                 # mult
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
-                out = self._get_out_idx(3)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
+                out = self._get_idx(3)
                 self.memory[out] = a * b
                 self.instruction_ptr += 4
 
             elif opcode == 3:
                 # input
-                out = self._get_out_idx(1)
+                out = self._get_idx(1)
                 self.memory[out] = self.input_list[self.input_ptr]
                 self.input_ptr += 1
                 self.instruction_ptr += 2
 
             elif opcode == 4:
                 # output
-                a = self._get_param(param_modes, 1)
+                a = self._get_value(param_modes, 1)
                 self.output_list.append(a)
                 print('intcode output: {}'.format(self.output_list[-1]))
                 self.state = self.STATE_OUTPUT
@@ -115,8 +115,8 @@ class IntcodeComputer(object):
 
             elif opcode == 5:
                 # jump if true
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
                 if a:
                     self.instruction_ptr = b
                 else:
@@ -124,8 +124,8 @@ class IntcodeComputer(object):
 
             elif opcode == 6:
                 # jump if false
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
                 if not a:
                     self.instruction_ptr = b
                 else:
@@ -133,23 +133,23 @@ class IntcodeComputer(object):
 
             elif opcode == 7:
                 # less than
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
-                out = self._get_out_idx(3)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
+                out = self._get_idx(3)
                 self.memory[out] = int(a < b)
                 self.instruction_ptr += 4
 
             elif opcode == 8:
                 # eq
-                a = self._get_param(param_modes, 1)
-                b = self._get_param(param_modes, 2)
-                out = self._get_out_idx(3)
+                a = self._get_value(param_modes, 1)
+                b = self._get_value(param_modes, 2)
+                out = self._get_idx(3)
                 self.memory[out] = int(a == b)
                 self.instruction_ptr += 4
 
             elif opcode == 9:
                 # adjust rel off
-                a = self._get_param(param_modes, 1)
+                a = self._get_value(param_modes, 1)
                 self.rel_offset += a
                 self.instruction_ptr += 2
 
@@ -162,7 +162,7 @@ class IntcodeComputer(object):
         while len(self.memory) <= index:
             self.memory.append(0)
 
-    def _get_out_idx(self, offset: int) -> int:
+    def _get_idx(self, offset: int) -> int:
         """
         write_index is the index we will be writing to
         """
@@ -181,7 +181,7 @@ class IntcodeComputer(object):
         self._expand_mem(write_index)
         return write_index
 
-    def _get_param(self, param_modes: typing.List[int], offset: int) -> int:
+    def _get_value(self, param_modes: typing.List[int], offset: int) -> int:
         try:
             param_mode = param_modes[offset - 1]
         except IndexError:
