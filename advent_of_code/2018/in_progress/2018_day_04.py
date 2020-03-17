4c577c11f74dcbf5a78e0ed1d46a3ba1a9390703
Scott Oehler <sdo91@gmx.com>
2020-03-17 09:30:12 -0600

    day 4 start

--- advent_of_code/2018/in_progress/2018_day_04.py
+++ advent_of_code/2018/in_progress/2018_day_04.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
+def addToPath(relPath):
+    from os import path
+    import sys
+    dirOfThisFile = path.dirname(path.realpath(__file__))
+    dirToAdd = path.normpath(path.join(dirOfThisFile, relPath))
+    if dirToAdd not in sys.path:
+        print('adding to path: {}'.format(dirToAdd))
+        sys.path.insert(0, dirToAdd)
+    else:
+        print('already in path: {}'.format(dirToAdd))
 
+addToPath('../..')
 
 ### IMPORTS ###
 
-import aocd
-import re
-import parse
-import typing
-import aoc_util
-from aoc_util import AocLogger
+# import numpy as np
 
+import aocd
+
+from aoc_util import aoc_util
+from aoc_util.aoc_util import AocLogger
 
 
 ### CONSTANTS ###
-TEST_INPUT = [
+TEST_INPUT_1 = [
     """
 
     """, """
@@ -30,11 +40,38 @@ TEST_OUTPUT_1 = [
     0,
 ]
 
+TEST_INPUT_2 = [
+    """
+
+    """, """
+
+    """, """
+
+    """
+]
+
 TEST_OUTPUT_2 = [
     0,
     0,
     0,
 ]
+
+
+
+
+
+class MyObject(object):
+
+    def __init__(self, text):
+        self.text = text
+        self.id = 0
+
+    def __str__(self):
+        return 'MyObject {}: {}'.format(
+            self.id, self.text)
+
+    def __repr__(self):
+        return str(self)
 
 
 
@@ -48,81 +85,57 @@ class AdventOfCode(object):
     def run(self):
         print('starting {}'.format(__file__.split('/')[-1]))
 
-        puzzle_input = 'todo: use real data'
-        # puzzle_input = aocd.data
+        try:
+            puzzle_input = aocd.data
+        except aocd.exceptions.AocdError:
+            puzzle_input = 'unable to get input'
         aoc_util.write_input(puzzle_input, __file__)
 
-        AocLogger.verbose = True
-        self.test_cases()
+        self.run_tests()
 
         AocLogger.verbose = False
+
         # self.solve_part_1(puzzle_input)
+
         # self.solve_part_2(puzzle_input)
 
-    def test_cases(self):
-        self.verbose = True
-        AocLogger.log()
-        AocLogger.log('running test cases')
+        # aoc_util.assert_equal(
+        #     0,
+        #     self.solve_part_1(puzzle_input)
+        # )
 
-        for test_in, test_out in zip(TEST_INPUT, TEST_OUTPUT_1):
-            test_in = test_in.strip()
-            if not test_in:
-                continue
+        # aoc_util.assert_equal(
+        #     0,
+        #     self.solve_part_2(puzzle_input)
+        # )
 
-            # do the test
-            aoc_util.assert_equal(
-                test_out,
-                self.solve_part_1(test_in)
-            )
+    def run_tests(self):
+        AocLogger.verbose = True
+        aoc_util.run_tests(self.solve_part_1, TEST_INPUT_1, TEST_OUTPUT_1)
+        # aoc_util.run_tests(self.solve_part_2, TEST_INPUT_2, TEST_OUTPUT_2)
 
-        # for test_in, test_out in zip(TEST_INPUT, TEST_OUTPUT_2):
-        #     test_in = test_in.strip()
-        #     if not test_in:
-        #         continue
-        #
-        #     # do the test
-        #     aoc_util.assert_equal(
-        #         test_out,
-        #         self.solve_part_2(test_in)
-        #     )
+    def solve_test_case_1(self, test_input):
+        AocLogger.log('test input: {}'.format(test_input))
+        return 0
 
-        AocLogger.log('all test cases passed')
-        AocLogger.log('\n' * 5)
+    def solve_part_1(self, puzzle_input: str):
+        puzzle_input = puzzle_input.strip()
 
-    def solve_part_1(self, puzzle_input):
-        """
-        Args:
-            puzzle_input (string): the input
-
-        Returns: the answer
-        """
-        lines = puzzle_input.strip().split('\n')
         result = 0
-        for line in lines:
-            line = line.strip()
 
-            # todo: process line here
-            AocLogger.log(line)
-
-        print('\npart 1 result: {}'.format(result))
+        print('part 1 result: {}'.format(result))
         return result
 
-    def solve_part_2(self, puzzle_input):
-        """
-        Args:
-            puzzle_input (string): the input
+    def solve_test_case_2(self, test_input):
+        AocLogger.log('test input: {}'.format(test_input))
+        return 0
 
-        Returns: the answer
-        """
-        lines = puzzle_input.strip().split('\n')
+    def solve_part_2(self, puzzle_input: str):
+        puzzle_input = puzzle_input.strip()
+
         result = 0
-        for line in lines:
-            line = line.strip()
 
-            # todo: process line here
-            AocLogger.log(line)
-
-        print('\npart 2 result: {}'.format(result))
+        print('part 2 result: {}'.format(result))
         return result
 
 
