cc9d030e1c0e3c3ab5a41a0316dec2c17efd7c3f
Scott Oehler <sdo91@gmx.com>
2020-03-17 23:02:21 -0600

    solved 18.5.2

--- advent_of_code/2018/in_progress/2018_day_05.py
+++ advent_of_code/2018/in_progress/2018_day_05.py
@@ -44,7 +44,7 @@ TEST_OUTPUT_1 = [
 ]
 
 TEST_OUTPUT_2 = [
-    0,
+    4,
     0,
     0,
 ]
@@ -83,10 +83,10 @@ class AdventOfCode(object):
             self.solve_part_1(puzzle_input)
         )
 
-        # aoc_util.assert_equal(
-        #     0,
-        #     self.solve_part_2(puzzle_input)
-        # )
+        aoc_util.assert_equal(
+            4052,
+            self.solve_part_2(puzzle_input)
+        )
 
         elapsed_time = time.time() - start_time
         print('elapsed_time: {:.3f} sec'.format(elapsed_time))
@@ -94,7 +94,7 @@ class AdventOfCode(object):
     def run_tests(self):
         AocLogger.verbose = True
         aoc_util.run_tests(self.solve_part_1, TEST_INPUT, TEST_OUTPUT_1)
-        # aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2)
+        aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2)
 
     def solve_part_1(self, puzzle_input: str):
         solver = Solver(puzzle_input)
@@ -139,31 +139,62 @@ class Solver(object):
             return a.islower() != b.islower()  # different polarity
         return False
 
-    def p1(self):
+    def p1(self, polymer=''):
         """
         dabAcCaCBAcCcaDA  The first 'cC' is removed.
         dabAaCBAcCcaDA    This creates 'Aa', which is removed.
         dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
         dabCBAcaDA        No further actions can be taken.
         """
+        if not polymer:
+            polymer = self.text
         builder = []
-        for char in self.text:
+        for char in polymer:
             if self.can_react(builder, char):
-                # pop
-                del builder[-1]
+                del builder[-1]  # pop
             else:
-                # push
-                builder.append(char)
+                builder.append(char)  # push
 
-        if AocLogger.verbose:
-            AocLogger.log('p1 builder: {}'.format(''.join(builder)))
+        # if AocLogger.verbose:
+        #     AocLogger.log('p1 builder: {}'.format(''.join(builder)))
         return len(builder)
 
     def p2(self):
         """
+        Time to improve the polymer.
 
+        One of the unit types is causing problems; it's preventing the polymer from collapsing as much as it should.
+        Your goal is to figure out which unit type is causing the most problems, remove all instances of it
+        (regardless of polarity), fully react the remaining polymer, and measure its length.
+
+        For example, again using the polymer dabAcCaCBAcCcaDA from above:
+
+        Removing all A/a units produces dbcCCBcCcD      -> dbCBcD, which has length 6.
+        Removing all B/b units produces daAcCaCAcCcaDA  -> daCAcaDA, which has length 8.
+        Removing all C/c units produces dabAaBAaDA      -> daDA, which has length 4.
+        Removing all D/d units produces abAcCaCBAcCcaA  -> abCBAc, which has length 6.
+        In this example, removing all C/c units was best, producing the answer 4.
+
+        What is the length of the shortest polymer you can produce by removing all units of exactly one type and
+        fully reacting the result?
         """
-        return 2
+        if 'z' in self.text:
+            last_char = 'z'
+        else:
+            last_char = 'd'
+
+        min_size = 9e9
+        for x in range(ord('a'), ord(last_char) + 1):
+            char = chr(x)
+            polymer = self.text.replace(char, '')
+            polymer = polymer.replace(char.upper(), '')
+            size = self.p1(polymer)
+
+            print('{}: {}'.format(char, size))
+            if size < min_size:
+                min_size = size
+
+        return min_size
 
 
 
