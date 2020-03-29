61b54a5232b9d622520b5788190beb31136967d9
Scott Oehler <sdo91@gmx.com>
2020-03-29 16:19:51 -0600

    18.11.1

--- advent_of_code/2018/in_progress/aoc_2018_11.py
+++ advent_of_code/2018/in_progress/aoc_2018_11.py
@@ -22,22 +22,23 @@ import aocd
 
 from advent_of_code.util import aoc_util
 from advent_of_code.util.aoc_util import AocLogger
+from advent_of_code.util.grid_2d import Grid2D
 
 
 ### CONSTANTS ###
 TEST_INPUT = [
     """
-
+18
     """, """
-
+42
     """, """
 
     """
 ]
 
 TEST_OUTPUT_1 = [
-    0,
-    0,
+    (33, 45),
+    (21, 61),
     0,
 ]
 
@@ -77,7 +78,7 @@ class AdventOfCode(object):
         AocLogger.verbose = False
 
         aoc_util.assert_equal(
-            0,
+            (44, 37),
             self.solve_part_1(puzzle_input)
         )
 
@@ -91,7 +92,14 @@ class AdventOfCode(object):
 
     def run_tests(self):
         AocLogger.verbose = True
+
+        aoc_util.assert_equal( 4, Solver.calc_power_level((  3,   5),  8))
+        aoc_util.assert_equal(-5, Solver.calc_power_level((122,  79), 57))
+        aoc_util.assert_equal( 0, Solver.calc_power_level((217, 196), 39))
+        aoc_util.assert_equal( 4, Solver.calc_power_level((101, 153), 71))
+
         aoc_util.run_tests(self.solve_part_1, TEST_INPUT, TEST_OUTPUT_1)
+
         # aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2)
 
     def solve_part_1(self, puzzle_input: str):
@@ -121,6 +129,8 @@ class AdventOfCode(object):
 
 class Solver(object):
 
+    ORDER = 300
+
     def __init__(self, text: str):
         self.text = text.strip()
         AocLogger.log(str(self))
@@ -133,8 +143,53 @@ class Solver(object):
         """
 
         """
-        z=0
-        return 1
+        serial_number = int(self.text)
+
+        grid = Grid2D()
+        grid.set_value_width(4)
+
+        max_sq_power = -99
+        max_coord = None
+
+        # pop grid
+        for y in range(1, self.ORDER + 1):
+            for x in range(1, self.ORDER + 1):
+                coord = x, y
+                power_level = self.calc_power_level(coord, serial_number)
+                grid.set_tuple(coord, power_level)
+
+                # check square power
+                top_left = (x - 2, y - 2)
+                sq_power = self.calc_square_power(grid, top_left)
+                if sq_power > max_sq_power:
+                    max_sq_power = sq_power
+                    max_coord = top_left
+
+        self.show_square(grid, max_coord)
+        print('max_sq_power: {}'.format(max_sq_power))
+        return max_coord
+
+    def calc_square_power(self, grid, top_left):
+        if min(top_left) < 1:
+            return -99
+
+        result = 0
+        for y_offset in range(3):
+            for x_offset in range(3):
+                coord = (top_left[0] + x_offset, top_left[1] + y_offset)
+                result += grid.get(*coord)
+        return result
+
+    def show_square(self, grid, top_left):
+        adj_top_left = (
+            top_left[0] - 1,
+            top_left[1] - 1
+        )
+        bottom_right = (
+            top_left[0] + 3,
+            top_left[1] + 3
+        )
+        grid.show_from(adj_top_left, bottom_right)
 
     def p2(self):
         """
@@ -142,6 +197,15 @@ class Solver(object):
         """
         z=0
         return 2
+
+    @classmethod
+    def calc_power_level(cls, coord, serial_number):
+        rack_id = coord[0] + 10
+        power_level = rack_id * coord[1] + serial_number
+        power_level *= rack_id
+        power_level = power_level % 1000 // 100  # get hundreds digit
+        power_level -= 5
+        return power_level
 
 
 
