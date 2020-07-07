dbef93bb9ecd6143cfa3436d82d12a9d142dd952
Scott Oehler <sdo91@gmx.com>
2020-07-07 15:17:48 -0600

    18.20 wip

--- advent_of_code/year_2018/in_progress/aoc_2018_20.py
+++ advent_of_code/year_2018/in_progress/aoc_2018_20.py
@@ -22,6 +22,7 @@ import aocd
 
 from advent_of_code.util import aoc_util
 from advent_of_code.util.aoc_util import AocLogger
+from advent_of_code.util.grid_2d import Grid2D
 
 
 ### CONSTANTS ###
@@ -32,6 +33,10 @@ TEST_INPUT = [
 ^ENWWW(NEEE|SSE(EE|N))$
     """, """
 ^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$
+    """, """
+^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$
+    """, """
+^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$
     """
 ]
 
@@ -39,9 +44,13 @@ TEST_OUTPUT_1 = [
     3,
     10,
     18,
+    23,
+    31,
 ]
 
 TEST_OUTPUT_2 = [
+    0,
+    0,
     0,
     0,
     0,
@@ -78,23 +87,24 @@ class AdventOfCode(object):
 
         AocLogger.verbose = False
 
-        aoc_util.assert_equal(
-            4050,
-            self.solve_part_1(self.puzzle_input)
-        )
-
         # aoc_util.assert_equal(
-        #     0,
-        #     self.solve_part_2(self.puzzle_input)
+        #     4050,
+        #     self.solve_part_1(self.puzzle_input)
         # )
+
+        aoc_util.assert_equal(
+            0,
+            self.solve_part_2(self.puzzle_input)
+        )
 
         elapsed_time = time.time() - start_time
         print('elapsed_time: {:.3f} sec'.format(elapsed_time))
 
     def run_tests(self):
         AocLogger.verbose = True
-        aoc_util.run_tests(self.solve_part_1, TEST_INPUT, TEST_OUTPUT_1)
-        # aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2)
+        # aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2, cases={1})
+        aoc_util.run_tests(self.solve_part_2, TEST_INPUT, TEST_OUTPUT_2)
+        # aoc_util.run_tests(self.solve_part_1, TEST_INPUT, TEST_OUTPUT_1)
 
     def solve_part_1(self, text: str):
         solver = Solver(text)
@@ -127,17 +137,23 @@ class Solver(object):
         self.text = text.strip()
         AocLogger.log(str(self))
 
+        # p2
+        self.grid = Grid2D('X')
+        self.surround((0, 0))
+        self.grid.show()
+        self.stamp = time.time()
+
     def __repr__(self):
         return '{}:\n{}\n'.format(
             type(self).__name__, self.text)
 
-    def process(self, text):
+    def process_p1(self, text):
         """
         algo:
             recursively process parens
 
             eg:
-                while there are perens:
+                while there are parens:
                     find open, close
                     pass to func, replace with result
 
@@ -155,7 +171,7 @@ class Solver(object):
             close_idx = self.find_close_paren(text, open_idx)
             inside = text[open_idx+1:close_idx]
             z=0
-            text = text[:open_idx] + self.process(inside) + text[close_idx + 1:]
+            text = text[:open_idx] + self.process_p1(inside) + text[close_idx + 1:]
             z=0
 
         # process pipes
@@ -188,15 +204,88 @@ class Solver(object):
 
     def p1(self):
         assert (self.text[0], self.text[-1]) == ('^', '$')
-        route = self.process(self.text[1:-1])
+        route = self.process_p1(self.text[1:-1])
         return len(route)
 
     def p2(self):
         """
 
         """
+        text = self.text[1:-1]
+        start_coord = (0, 0)
+        self.draw_map(text, start_coord)
+        self.grid.replace('?', '#')
+        self.grid.show()
         z=0
-        return 2
+        return 0
+
+    def draw_map(self, text, start_coord):
+        """
+        algo:
+
+            given a starting point
+
+            if we hit a paren:
+                find start/end
+                call recursive (return coord)
+                if there are more chars,
+                    make sure coord is the same...
+                    resume after end
+
+            if we hit a pipe:
+                reset back to start coord
+
+
+
+        """
+        i = 0
+        current_coord = start_coord
+        text_len = len(text)
+        while i < text_len:
+
+            char = text[i]
+
+            if char in Grid2D.DIRECTIONS:
+                current_coord = Grid2D.get_coord_direction(current_coord, char)
+                door = '|' if char in Grid2D.EAST_WEST else '-'
+                self.grid.set_tuple(current_coord, door)
+                current_coord = Grid2D.get_coord_direction(current_coord, char)
+                self.grid.set_tuple(current_coord, '.')
+                self.surround(current_coord)
+
+            elif char == '(':
+                open_idx = i
+                close_idx = self.find_close_paren(text, open_idx)
+                inside = text[open_idx+1:close_idx]
+                self.draw_map(inside, current_coord)
+                i = close_idx
+
+            elif char == '|':
+                # reset current coord
+                current_coord = start_coord
+
+            else:
+                assert False
+
+            now = time.time()
+            if now - self.stamp > 5:
+                self.stamp = now
+                self.grid.show(current_coord)
+            i += 1
+
+        return current_coord
+
+
+
+    def surround(self, coord):
+        self.grid.set_coords_default(Grid2D.get_adjacent_coords(coord), '?')
+        self.grid.set_coords_default(Grid2D.get_diagonal_coords(coord), '#')
+
+
+
+
+
+
 
 
 
