da0f8e8597b65dc46510202e91a10b4e7f307dbe
Scott Oehler <sdo91@gmx.com>
2020-07-09 13:43:35 -0600

    refactored 17.1 solution

--- aoc_go/src/aoc_main.go
+++ aoc_go/src/aoc_main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"aoc_go/util"
-	"aoc_go/y17"
+	"aoc_go/year17"
 	"fmt"
 )
 
 func main()  {
-
 	fmt.Println("starting app")
 
-	util.DoSomething()
-
-	y17.SolveDay1()
+	year17.SolveDay01()
 
 	fmt.Println("done")
-
 }
--- aoc_go/src/util/aoc_go_util.go
+++ aoc_go/src/util/aoc_go_util.go
@@ -2,8 +2,33 @@ package util
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func DoSomething() {
-	fmt.Println("doing something")
+type aocSolverFunc func(string) int
+
+func TestSolver(solver aocSolverFunc, input string, expectedOutput int) {
+	actualOutput := solver(input)
+	if actualOutput != expectedOutput {
+		err := fmt.Sprintf("f(%v) returns %v, expected %v", input, actualOutput, expectedOutput)
+		panic(err)
+	}
+}
+
+func ToDigits(text string) []int {
+	result := []int{}
+	// fmt.Println(result)
+
+	for _, c := range text {
+		asChar := string(c)
+		asDigit, err := strconv.Atoi(asChar)
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("%v, %c, %v, %v \n", i, c, asChar, asDigit)
+		result = append(result, asDigit)
+	}
+
+	// fmt.Println(result)
+	return result
 }
